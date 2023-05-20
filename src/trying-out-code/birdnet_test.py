fd3401d67c0db42ee5e7048186202faf04f245cd
JamesSkinna <james.skinner19@imperial.ac.uk>
2023-05-20 16:59:20 +0100

    Added channel extraction & birdNET processing

--- src/trying-out-code/birdnet_test.py
+++ src/trying-out-code/birdnet_test.py
@@ -7,9 +7,9 @@ analyzer = Analyzer()
 
 recording = Recording(
     analyzer,
-    "data/SpecificTests/OverlappingBirds_Silwood/Channel1.mp3",
-    lat=51.40644012,        # Silwood Lat & Long
-    lon=-0.63814066,
+    "data/SpecificTests/Controlled_Test/mixkit-melodic-songbird-chirp-in-the-wild-68.wav",
+    # lat=51.40644012,        # Silwood Lat & Long
+    # lon=-0.63814066,
     date=datetime(year=2023, month=4, day=25), # use date or week_48
     min_conf=0.25,
 )
