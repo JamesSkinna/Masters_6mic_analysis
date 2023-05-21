a4be123ad901907d4907ea43be4f53a5474f07b7
JamesSkinna <james.skinner19@imperial.ac.uk>
2023-05-21 17:16:46 +0100

    Added multiprocessing

--- src/trying-out-code/birdnet_test.py
+++ src/trying-out-code/birdnet_test.py
@@ -7,8 +7,8 @@ analyzer = Analyzer()
 
 recording = Recording(
     analyzer,
-    # "data/SpecificTests/OverlappingBirds_Silwood/chan6.mp3",
-    "data/SpecificTests/OverlappingBirds_Silwood/6Channels/sep_chan4.mp3",
+    # "data/SpecificTests/OverlappingBirds_Silwood/chan1.mp3",
+    "data/SpecificTests/OverlappingBirds_Silwood/6Channels/original_chan1.mp3",
     lat=51.409111,        # Silwood Lat & Long
     lon=-0.637820,
     date=datetime(year=2023, month=5, day=10), # use date or week_48
